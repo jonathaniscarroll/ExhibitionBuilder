fdeccfd0cb0d196b9ac156f8fdd7bbb7d278c7ed
Jonathan Edward Carroll <jonathaniscarroll@gmail.com>
2024-06-12 11:36:46 -0300

    added text object

---
+++ Assets/SetText.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetText : MonoBehaviour
+{
+	[SerializeField]
+	private string _text;
+	public string text {
+		get{return _text;}
+		set{
+			_text = value;
+			OutputText.Invoke(_text);
+		}
+	}
+	public StringEvent OutputText;
+}
